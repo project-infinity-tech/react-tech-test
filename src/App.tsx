54cf216611686f17c6c61296b022805eb1d659b1
Livia Rett <livia.rett@projectinfinity.tech>
2024-07-04 13:50:31 +0100

    add Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,82 @@
-import React, { CSSProperties, FC } from 'react';
-import { fetchComponents } from './data';
+import React, { CSSProperties, FC, Suspense } from "react";
+import { DBComponent, fetchData } from "./data";
+const resource = fetchData("task-2", 2000);
 
-const Component: FC<{ tag: string, content: string, style?: CSSProperties }> = ({ tag, content, ...props }) => {
-  return React.createElement(tag, props, [
-    content,
-  ]);
+const Component: FC<{
+  tag: string;
+  content?: DBComponent["content"];
+  style?: CSSProperties;
+}> = ({ tag, content, ...props }) => {
+  if (!content) {
+    return null;
+  }
+
+  if ("type" in content) {
+    return React.createElement(tag, props, [content.value]);
+  }
+
+  return React.createElement(
+    tag,
+    props,
+    content.map((props) =>
+      React.createElement(Component, { ...props, key: props.id })
+    )
+  );
 };
 
-function App() {
-  // TODO this should maybe fetch some data from our database...?
-  const data = fetchComponents("task-1");
+function Components() {
+  const data = resource.read() ?? [];
+
+  const componentTree = data.reduce<DBComponent[]>((acc, component) => {
+    if (component.parentId) {
+      const parent = data.find((c) => c.id === component.parentId);
+
+      if (!parent) {
+        if (!acc.find((c) => c.id === component.id)) {
+          return acc.concat({ ...component });
+        } else {
+          return acc;
+        }
+      }
+
+      let parentIndex = acc.findIndex((c) => c.id === parent.id);
+
+      if (parentIndex === -1) {
+        acc.push({ ...parent });
+        parentIndex = acc.length - 1;
+      }
+
+      const parentComponent = acc[parentIndex];
+
+      if (parentComponent.content && Array.isArray(parentComponent.content)) {
+        parentComponent.content.push(component);
+      } else {
+        parentComponent.content = [component];
+      }
+      return acc.filter((c) => c.id !== component.id);
+    } else {
+      if (!acc.find((c) => c.id === component.id)) {
+        return acc.concat({ ...component });
+      } else {
+        return acc;
+      }
+    }
+  }, []);
 
   return (
     <div>
-      <Component tag="h1" content="Hello" />
-      <Component tag="h2" content="Static" style={{ fontStyle: "italic" }} />
+      {componentTree.map(({ content, ...props }) => {
+        return <Component key={props.id} content={content} {...props} />;
+      })}
     </div>
   );
 }
 
+function App() {
+  return (
+    <Suspense fallback={<p>Loading.....</p>}>
+      <Components />
+    </Suspense>
+  );
+}
 export default App;
--- src/data.ts
+++ src/data.ts
@@ -1,52 +1,87 @@
 import { CSSProperties } from "react";
 
+export type DBComponent = {
+  id: string;
+  tag: string;
+  content?: { type: string; value: string } | DBComponent[];
+  parentId?: string;
+  style?: CSSProperties;
+};
+
 // Imagine this is stored as rows / documents in a remote database...
-const database = {
+const database: Record<string, DBComponent> = {
   comp1: {
-    id: 'comp1',
-    tag: 'h1',
+    id: "comp1",
+    tag: "h1",
     content: {
-      type: 'text',
-      value: 'Hello',
+      type: "text",
+      value: "Hello",
     },
+    parentId: "comp3",
   },
   comp2: {
-    id: 'comp2',
-    tag: 'h2',
+    id: "comp2",
+    tag: "h2",
     content: {
-      type: 'text',
-      value: 'Database!',
+      type: "text",
+      value: "Database!",
     },
     style: {
-      fontStyle: 'italic',
-    } as CSSProperties,
+      fontStyle: "italic",
+    },
+    parentId: "comp3",
   },
   comp3: {
-    id: 'comp3',
-    tag: 'div',
+    id: "comp3",
+    tag: "div",
     style: {
-      border: '1px dashed black',
-    } as CSSProperties,
-    content: '...'
-  }
-}
+      border: "1px dashed black",
+    },
+  },
+};
 
 // And this is our function for querying them
 export const fetchComponents = async (taskId: string) => {
-  if (taskId === 'task-1') {
-    return [
-      database.comp1,
-      database.comp2,
-    ];
+  if (taskId === "task-1") {
+    return [database.comp1, database.comp2];
   }
 
-  if (taskId === 'task-2') {
-    return [
-      database.comp1,
-      database.comp2,
-      database.comp3,
-    ];
+  if (taskId === "task-2") {
+    return [database.comp1, database.comp2, database.comp3];
   }
 
-  throw new Error('Task not found');
-}
+  throw new Error("Task not found");
+};
+
+export const fetchData = (taskId: string, artificialDelay: number) => {
+  let status = "pending";
+  let result: DBComponent[] = [];
+
+  let suspender = new Promise<void>((resolve, reject) => {
+    setTimeout(() => {
+      if (taskId === "task-1") {
+        result = [database.comp1, database.comp2];
+      } else if (taskId === "task-2") {
+        result = [database.comp1, database.comp2, database.comp3];
+      } else {
+        status = "error";
+        reject();
+      }
+
+      status = "success";
+      resolve();
+    }, artificialDelay);
+  });
+
+  return {
+    read() {
+      if (status === "pending") {
+        throw suspender;
+      } else if (status === "error") {
+        throw result;
+      } else if (status === "success") {
+        return result;
+      }
+    },
+  };
+};
