ed9a8884d526c29cb887a3d244efb61be1928bec
alexfaunt <alexfaunt@gmail.com>
2024-07-05 12:13:32 +0100

    Add type for data source

--- src/data.ts
+++ src/data.ts
@@ -1,38 +1,39 @@
 import { CSSProperties } from "react";
 
+export type SerializedComponent = {
+  id: string,
+  tag: string,
+  content?: string,
+  style?: CSSProperties,
+}
+
 // Imagine this is stored as rows / documents in a remote database...
 const database = {
   comp1: {
     id: 'comp1',
     tag: 'h1',
-    content: {
-      type: 'text',
-      value: 'Hello',
-    },
+    content: 'Hello',
   },
   comp2: {
     id: 'comp2',
     tag: 'h2',
-    content: {
-      type: 'text',
-      value: 'Database!',
-    },
+    content: 'Database!',
     style: {
       fontStyle: 'italic',
-    } as CSSProperties,
+    }
   },
+
   comp3: {
     id: 'comp3',
     tag: 'div',
     style: {
       border: '1px dashed black',
-    } as CSSProperties,
-    content: '...'
+    },
   }
 }
 
-// And this is our function for querying them
-export const fetchComponents = async (taskId: string) => {
+// And this is our function for querying them from the remote database
+export const fetchComponents = async (taskId: string): Promise<SerializedComponent[]> => {
   if (taskId === 'task-1') {
     return [
       database.comp1,
